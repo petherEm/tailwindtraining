2f4988c2785e8162aa54b1f8c3b1154e1c6ac746
Piotr Maciejewski <pm@piotrs-macbook-pro.home>
2022-08-26 19:21:39 +0200

    udpate Aug22

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Features from '../components/Features'
 import Testimonials from '../components/Testimonials'
 import Cta from '../components/Cta'
 import Footer from '../components/Footer'
+import Mymodal from '../components/Mymodal'
 
 
 export default function Home() {
@@ -14,11 +15,12 @@ export default function Home() {
       <Head>
         <title>Tailwind Training</title>
         <meta name="description" content="Tailwind training" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.ico" />
       </Head>
 
       <Navbar />
       <Hero />
+      <Mymodal />
       <Features />
       <Testimonials />
       <Cta />
